169dbac4be75a32f9658af236fd906a5d16b74cb
shubham <shubhamkumar10516@gmail.com>
2020-09-27 23:44:40 +0530

    [Shubham]ADDED: UC1 welcome message printing file

---
+++ AddressBookMain.java
@@ -0,0 +1,12 @@
+package com.academy.AddressBookMain;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		// Displaying Welcome message
+		
+		System.out.println(".....Welcome to Address book....");
+
+	}
+
+}
